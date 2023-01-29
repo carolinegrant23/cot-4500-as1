be570d98161e9ffd8650593068edde5ee6a7c769
Caroline Grant <carolinegrant@knights.ucf.edu>
2023-01-28 23:05:05 -0500

    completes the assignment

--- src/main/assignment_1.py
+++ src/main/assignment_1.py
@@ -1,5 +1,6 @@
 import numpy as np
 import struct
+import math
 
 
 binary_number = 0b0100000001111110101110010000000000000000000000000000000000000000
@@ -8,15 +9,18 @@ buffer = binary_number.to_bytes(8,'big')
 
 #question 1: 
 double_precision = struct.unpack('>d', buffer)[0]
-print(f'{double_precision:.5f}')
+print(f'{double_precision:.4f}')
+print()
 
 #question 2:
-chop = int(double_precision)
+chop = float(int(double_precision))
 print(chop)
+print()
 
 #question 3:
 round_ = round(double_precision, 0)
 print(round_)
+print()
 
 #question 4: 
 absolute_error = (abs(double_precision - round_))
@@ -24,6 +28,7 @@ print(absolute_error)
 
 relative_error = (absolute_error / double_precision)
 print(relative_error)
+print()
 
 #question 5: 
 k = x = 1
@@ -35,6 +40,7 @@ while delta > ERROR :
     delta = abs(value)
     k += 1
 print(k-2)
+print()
 
 #question 6 part a:
 tol = 10**-4
@@ -53,35 +59,34 @@ while(abs(right - left) > tol):
     else:
         left = p
 print(i)
+print()
 
 #question 6 part b: 
 
 def custom_derivative(value):
-    return (3 * value* value) - (2 * value)
+    return (3 * value* value) + (8 * value)
 
 def f(x):
     return x**3 + 4*(x**2) - 10
 
-def polynomial_function(f):
-    f = polynomial_function()
-
-
-def newton_raphson(initial_approximation: float, tolerance: float, sequence: str):
+def newton_raphson(initial_approximation: float, tolerance: float):
     iteration_counter = 0
     x = initial_approximation
-    f_x = f(sequence)
+    f_x = f(x)
     f_prime = custom_derivative(initial_approximation)
-    
-    approximation: float = f / f_prime
+    approximation: float = f_x / f_prime
     while(abs(approximation) >= tolerance):
         x = initial_approximation
-        f_x = f(sequence)
+        f_x = f(x)
         f_prime = custom_derivative(initial_approximation)
-        approximation = f / f_prime
+        approximation = f_x / f_prime
         initial_approximation -= approximation
         iteration_counter += 1
+    return iteration_counter 
+
 if __name__ == "__main__":
-    initial_approximation: float = 1
+    initial_approximation: float = 7
     tolerance: float = .0001
-    sequence: str = "(x**3) + (4(x**2)) - 10"
-    newton_raphson(initial_approximation, tolerance, sequence)
+    result = newton_raphson(initial_approximation, tolerance)
+    print(result)
+    print()
