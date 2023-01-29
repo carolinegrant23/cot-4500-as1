02565f44aed28a4ab31cdc28fde78faae8e046fb
Caroline Grant <carolinegrant@knights.ucf.edu>
2023-01-28 20:50:01 -0500

    update 1/28

--- src/main/assignment_1.py
+++ src/main/assignment_1.py
@@ -19,7 +19,7 @@ round_ = round(double_precision, 0)
 print(round_)
 
 #question 4: 
-absolute_error = (double_precision - round_)
+absolute_error = (abs(double_precision - round_))
 print(absolute_error)
 
 relative_error = (absolute_error / double_precision)
@@ -27,8 +27,61 @@ print(relative_error)
 
 #question 5: 
 k = x = 1
-# while True:
-for i in range(10):
-    series = ((-1)**k * (x**k / k**3)) 
-    print(k, x, series)
+value = ((-1)**k * (x**k / k**3))
+delta = abs(value)
+ERROR = 10**-4
+while delta > ERROR :
+    value = ((-1)**k * (x**k / k**3))
+    delta = abs(value)
     k += 1
+print(k-2)
+
+#question 6 part a:
+tol = 10**-4
+left = -4
+right = 7
+def f(x):
+    return x**3 + 4*(x**2) - 10
+
+
+i = 0
+while(abs(right - left) > tol):
+    i += 1
+    p = (left + right) / 2
+    if((f(left) < 0 and f(p) > 0) or (f(left) > 0 and f(p) < 0)):
+        right = p 
+    else:
+        left = p
+print(i)
+
+#question 6 part b: 
+
+def custom_derivative(value):
+    return (3 * value* value) - (2 * value)
+
+def f(x):
+    return x**3 + 4*(x**2) - 10
+
+def polynomial_function(f):
+    f = polynomial_function()
+
+
+def newton_raphson(initial_approximation: float, tolerance: float, sequence: str):
+    iteration_counter = 0
+    x = initial_approximation
+    f_x = f(sequence)
+    f_prime = custom_derivative(initial_approximation)
+    
+    approximation: float = f / f_prime
+    while(abs(approximation) >= tolerance):
+        x = initial_approximation
+        f_x = f(sequence)
+        f_prime = custom_derivative(initial_approximation)
+        approximation = f / f_prime
+        initial_approximation -= approximation
+        iteration_counter += 1
+if __name__ == "__main__":
+    initial_approximation: float = 1
+    tolerance: float = .0001
+    sequence: str = "(x**3) + (4(x**2)) - 10"
+    newton_raphson(initial_approximation, tolerance, sequence)
